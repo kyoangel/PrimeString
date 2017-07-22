fa871a3a8aa818c51686f77bcf0712f343ff6e54
kyoangel <gd92x2@hotmail.com>
2017-07-22 15:37:47 +0800

    add some comment for logic

--- RecursiveTry/Program.cs
+++ RecursiveTry/Program.cs
@@ -24,45 +24,64 @@ namespace RecursiveTry
 
         public static bool PrimeString(string s)
         {
+            // empty string is prime
             if (string.IsNullOrEmpty(s))
             {
                 return true;
             }
 
+            // one char string is prime
             if (s.Length < 2)
             {
                 return true;
             }
             else
             {
-                return RecursiveIsPrime(s.Substring(0, 1), s.Substring(1));
+                return RecursiveTestPrime(s.Substring(0, 1), s.Substring(1));
             }
         }
 
-        private static bool RecursiveIsPrime(string a, string b)
+        /// <summary>
+        /// separate whole string into two parts, Divisor:a and Dividend:b
+        /// </summary>
+        /// <param name="a">Divisor</param>
+        /// <param name="b">Dividend</param>
+        /// <returns></returns>
+        private static bool RecursiveTestPrime(string a, string b)
         {
             if (b.Length < a.Length)
                 return true;
 
+            // b made by a means Not prime
             if (RecursiveIsBMadeByA(a, b))
             {
                 return false;
             }
             else
             {
-                return RecursiveIsPrime(a + b.Substring(0, 1), b.Substring(1));
+                // test longer 'a' and shorter 'b'
+                return RecursiveTestPrime(a + b.Substring(0, 1), b.Substring(1));
             }
         }
 
+        /// <summary>
+        /// check if 'b' is just build by 'a' repeatedly
+        /// </summary>
+        /// <param name="a">Divisor</param>
+        /// <param name="b">Dividend</param>
+        /// <returns></returns>
         private static bool RecursiveIsBMadeByA(string a, string b)
         {
+            // stop condition if b is shorter than a
             if (b.Length < a.Length)
                 return false;
+            // if length 'b' Divided by 'a' has Remainder
             if ((b.Length % a.Length) != 0)
                 return false;
 
             if (b.Substring(0, a.Length).Equals(a))
             {
+                // recursive end condition, make sure b is made by a
                 if (b.Length == a.Length)
                     return true;
                 return RecursiveIsBMadeByA(a, b.Substring(a.Length));
