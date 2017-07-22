f377a861f33ee7b34d26204e7a6d947f63aa0b49
kyo <kyoangel@users.noreply.github.com>
2017-07-22 14:22:14 +0800

    Kata.cs

---
+++ Kata.cs
@@ -0,0 +1,13 @@
+namespace myjinxin
+{
+    using System;
+    
+    public class Kata
+    {
+        public bool PrimeString(string s){
+          //coding and coding..
+          
+          
+        }
+    }
+}
